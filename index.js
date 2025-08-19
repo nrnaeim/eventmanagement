a9241ca01c3b403b81db845b5ab148a6c4c5fadc
Niemur Rahman <nrnaeim@gmail.com>
2025-08-20 00:56:26 +0600

    Main files configuration is completed

--- index.js
+++ index.js
@@ -9,3 +9,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./src/app");
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+//Connectinf to DB and Listening app
+mongoose.connect(MONGO_URI).then(() => {
+  console.log("Data Base connected successfully");
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
