122a026df2a7d5ef8aba2a0c1b1aa1f6730a9c49
Niemur Rahman <nrnaeim@gmail.com>
2025-08-20 03:53:56 +0600

    User model
    User model is created with validation for user field

---
+++ src/models/user.model.js
@@ -0,0 +1,64 @@
+/*
+ * Title: User model
+ * Description: This model will define user data model
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 2025/08/20
+ */
+//Dependencies
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [3, "Min lenght of name is 3"],
+      maxlength: [50, "Max lenght of name is 50"],
+      match: [/^[a-z ]{3,50}$/i, "Name can contains only letters and space"],
+    },
+    email: {
+      type: String,
+      requied: [true, "Email is required"],
+      unique: [true, "Email already exist"],
+      tolowercase: true,
+      match: [/^[a-z0-9]+@[a-z]+\.[a-z]{2,}$/i, "Invalid email"],
+    },
+
+    phoneNumber: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: [true, "Phone number already exist"],
+      match: [/^01[3-9]{1}[0-9]{8}$/, "Please provide a valid phone number"],
+    },
+    password: {
+      type: String,
+      requied: [true, "Password is required"],
+      minlength: [6, "Min password lenght is 6"],
+      maxlength: [32, "Max password lenght is 50"],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@!$%&])[A-Za-z\d@!$%&]{6,32}$/,
+        "Not a strong password",
+      ],
+    },
+    confirmPassword: {
+      type: String,
+      requied: [true, "Password is required"],
+      validate: {
+        validator: function (val) {
+          return val === this.password;
+        },
+        message: "Password not match",
+      },
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  this.confirmPassword = "undefined";
+});
