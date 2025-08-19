d4b2a8fd27c2def98253f6dfbc0596f37c427250
Niemur Rahman <nrnaeim@gmail.com>
2025-08-19 23:12:22 +0600

    basic app configurations is made and api file is created

---
+++ src/app.js
@@ -0,0 +1,23 @@
+/*
+ * Title: App
+ * Description:
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date:2025/08/19
+ */
+
+//Depndencies
+const express = require("express");
+
+//App instance
+const app = express();
+
+//Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//Route config
+
+//Exporting app
+module.exports = app;
---
+++ src/routes/api.v1.js
@@ -0,0 +1,15 @@
+/*
+ * Title: API file
+ * Description:
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 2025/08/19
+ */
+//Dependencies
+const express = require("express");
+
+//API instance
+const apiV1 = express.Router();
+
+//Exporting API
+module.exports = apiV1;
