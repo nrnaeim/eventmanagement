68f5e102b803926b5d0ea97313da00282a14af45
Niemur Rahman <nrnaeim@gmail.com>
2025-08-21 01:11:53 +0600

    User model email validator
    Email validator lowercase criteria fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Email is required"],
       unique: [true, "Email already exist"],
-      tolowercase: true,
+      lowercase: true,
       match: [/^[a-z0-9._-]+@[a-z]+\.[a-z]{2,}$/i, "Invalid email"],
     },
 
