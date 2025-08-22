e7e73500ba1e469d6e514ca8acc5de90b47bcfed
Niemur Rahman <nrnaeim@gmail.com>
2025-08-22 06:07:57 +0600

    Update api.v1.js
    authentication middleware is added to user route api

--- src/api/api.v1.js
+++ src/api/api.v1.js
@@ -9,13 +9,14 @@
 const express = require("express");
 const authRoute = require("../routes/auth.route");
 const userRoute = require("../routes/user.route");
+const middleware = require("../middlewares/auth.middelware");
 
 //API instance
 const apiV1 = express.Router();
 
 //Route configuration
 apiV1.use("/auth", authRoute);
-apiV1.use("/users", userRoute);
+apiV1.use("/users", middleware.authentication, userRoute);
 
 //Exporting API
 module.exports = apiV1;
