27855ad6bbee603c49e812e8c83b1a5f843b6203
Niemur Rahman <nrnaeim@gmail.com>
2025-08-23 03:05:09 +0600

    Create object id
    Monsgoose inported and a  mdethod is added to create new ObjectId from id string

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,6 +5,15 @@
  * Email: nrnaeim@gmail.com
  * Date:2025/08/22
  */
+//Dependencies
+const mongoose = require("mongoose");
+
+//Ensure array
 exports.ensureArray = (error) => {
   return Array.isArray(error) ? error : [error];
+};
+
+//new Object ID creator from string
+exports.newObjectId = (id) => {
+  return mongoose.Types.ObjectId.createFromHexString(id);
 };
