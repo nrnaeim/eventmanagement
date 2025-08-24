d217bfc6b6e2e7d1e5feee9a77d8744cd95a0784
Niemur Rahman <nrnaeim@gmail.com>
2025-08-24 16:33:19 +0600

    Event model update
    Event schema is updated. createdBy, timestamps fields are added

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -9,47 +9,58 @@
 const mongoose = require("mongoose");
 
 //Event Schema
-const eventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"],
-  },
-  description: {
-    type: String,
-    minlength: [10, "Description must have at least 10 characters"],
-  },
-  date: {
-    type: Date,
-    required: [true, "Event date is required"],
-    validate: {
-      validator: function (value) {
-        return value.getTime() > Date.now();
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      minlength: [10, "Description must have at least 10 characters"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Event date is required"],
+      validate: {
+        validator: function (value) {
+          return value.getTime() > Date.now();
+        },
+        message: "Date must be in future",
       },
-      message: "Date must be in future",
+    },
+    time: {
+      type: String,
+      required: [true, "Time is required"],
+      validate: {
+        validator: function (value) {
+          return /^(0?[1-9]|1[0-2]):([0-5][0-9]) (AM|PM)$/i.test(value);
+        },
+        message: "Please provide 'HH:MM' AM/PM format",
+      },
+    },
+    location: {
+      type: String,
+      required: [true, "Location is required"],
+    },
+    organizerName: {
+      type: String,
+      required: [true, "Organization name is required"],
+    },
+    eventBanner: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "User id is required"],
     },
   },
-  time: {
-    type: String,
-    required: [true, "Time is required"],
-    validate: {
-      validator: function (value) {
-        return /^(0?[1-9]|1[0-2]):([0-5][0-9]) (AM|PM)$/i.test(value);
-      },
-      message: "Please provide 'HH:MM' AM/PM format",
-    },
-  },
-  location: {
-    type: String,
-    required: [true, "Location is required"],
-  },
-  organizerName: {
-    type: String,
-    required: [true, "Organization name is required"],
-  },
-  eventBanner: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 //Definig event model
 const Event = mongoose.model("event", eventSchema);
