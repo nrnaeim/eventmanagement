9f6d9930fa990ab7076816065d4d0da199c54096
Niemur Rahman <nrnaeim@gmail.com>
2025-08-24 13:53:36 +0600

    Event schema is created
    Event schema and field validation is created and exported the mode from schema

---
+++ src/models/event.model.js
@@ -0,0 +1,58 @@
+/*
+ * Title: Event model
+ * Description:
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 2025/08/23
+ */
+//Dependencies
+const mongoose = require("mongoose");
+
+//Event Schema
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  description: {
+    type: String,
+    minlength: [10, "Description must have at least 10 characters"],
+  },
+  date: {
+    type: Date,
+    required: [true, "Event date is required"],
+    validate: {
+      validator: function (value) {
+        return value.getTime() > Date.now();
+      },
+      message: "Date must be in future",
+    },
+  },
+  time: {
+    type: String,
+    required: [true, "Time is required"],
+    validate: {
+      validator: function (value) {
+        return /^(0?[1-9]|1[0-2]):([0-5][0-9]) (AM|PM)$/i.test(value);
+      },
+      message: "Please provide 'HH:MM' AM/PM format",
+    },
+  },
+  location: {
+    type: String,
+    required: [true, "Location is required"],
+  },
+  organizerName: {
+    type: String,
+    required: [true, "Organization name is required"],
+  },
+  eventBanner: {
+    type: String,
+  },
+});
+
+//Definig event model
+const Event = mongoose.model("event", eventSchema);
+
+//Exporting model
+module.exports = Event;
