28dc2fd60bb2ddfe7eaf7d22186fa215d9eaec85
Niemur Rahman <nrnaeim@gmail.com>
2025-08-25 16:52:56 +0600

    Event controller update
    Create v\event controller is created

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -6,18 +6,39 @@
  * Date: 2025/08/24
  */
 //Depnendesncies
+const EventModel = require("../models/event.model");
 
 //Create Event
-exports.create = async (req, res) => {};
+exports.create = async (req, res, next) => {
+  try {
+    req.body.createdBy = req.payload._id;
+    req.body.eventBanner = req.file?.path;
+
+    const newEvent = await EventModel.create(req.body);
+
+    if (!newEvent) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Failed to create event" });
+    }
+    return res
+      .status(201)
+      .json({ success: true, message: "Event created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
 
 //Read All Events
-exports.getAll = async (req, res) => {};
+exports.getAll = async (req, res, next) => {
+  res.send("Hello from event controller");
+};
 
 //Read Single Event
-exports.get = async (req, res) => {};
+exports.get = async (req, res, next) => {};
 
 //Update Event
-exports.update = async (req, res) => {};
+exports.update = async (req, res, next) => {};
 
 //Delete Event
-exports.delete = async (req, res) => {};
+exports.delete = async (req, res, next) => {};
