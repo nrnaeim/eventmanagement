538ebd663efab8eacc7270e41335b18612ca6979
Niemur Rahman <nrnaeim@gmail.com>
2025-08-25 14:27:42 +0600

    Error handler function update
    Now error are hande with error instanceof instead of error.name

--- src/handlers/error.handler.js
+++ src/handlers/error.handler.js
@@ -5,56 +5,68 @@
  * Email: nrnaeim@gmail.com
  * Date: 2025/08/20
  */
-const errorHandler = (error) => {
-  //Validation error handler
-  if (error.name === "ValidationError") {
-    const errors = Object.keys(error.errors).map(
-      (key) => error.errors[key].properties.message
-    );
+const mongoose = require("mongoose");
+const JWT = require("jsonwebtoken");
 
-    return {
-      code: 400,
-      error: errors,
-    };
-  }
+const errorHandler = async (error, req, res, next) => {
+  try {
+    //Validation error handler
+    if (error instanceof mongoose.Error.ValidationError) {
+      const errors = Object.keys(error.errors).map(
+        (key) => error.errors[key].properties.message
+      );
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        error: errors,
+      });
+    }
 
-  //MongooseError
-  if (error.name === "MongooseError") {
-    return {
-      code: 400,
-      error: error.message,
-    };
-  }
+    //MongooseError
+    if (error instanceof mongoose.Error.CastError) {
+      return res.status(400).json({
+        success: false,
+        error: [error.message],
+      });
+    }
 
-  //MongoServerError
-  if (error.name === "MongoServerError" && error.code === 11000) {
-    const errors = Object.keys(error.keyValue).map(
-      (key) => `${error.keyValue[key]} already exist`
-    );
-    return {
-      code: 400,
-      error: errors,
-    };
-  }
+    //MongoServerError
+    if (error.name === "MongoServerError" && error.code === 11000) {
+      const errors = Object.keys(error.keyValue).map(
+        (key) => `${error.keyValue[key]} already exist`
+      );
+      return res.status(400).json({
+        success: false,
+        error: errors,
+      });
+    }
 
-  //Json webtoken error
-  if (
-    error.name === "JsonWebTokenError" ||
-    error.name === "TokenExpiredError" ||
-    error.name === "NotBeforeError"
-  ) {
-    return {
-      code: 400,
-      error: `${error.message.charAt(0).toUpperCase()}${error.message.slice(
-        1
-      )}`,
-    };
+    //Json webtoken error
+    if (
+      error instanceof JWT.JsonWebTokenError ||
+      error instanceof JWT.TokenExpiredError ||
+      error instanceof JWT.NotBeforeError
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Failed to authenticate",
+        error: [
+          `${error.message.charAt(0).toUpperCase()}${error.message.slice(1)}`,
+        ],
+      });
+    }
+
+    //Default error handler
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
-  //Default error handler
-  return {
-    code: 500,
-    error: "Internal server error",
-  };
 };
 
 //Exporting error handler
